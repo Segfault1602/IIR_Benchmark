712f7e33192535849dca69740ec699fc4206b3cf
Segfault1602 <stonge.alex@gmail.com>
2025-04-27 13:30:46 -0700

    Fix build on Windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <fstream>
 #include <iostream>
 #include <random>
@@ -7,9 +8,13 @@
 #include "filter.h"
 
 #include "basic_filter.h"
+
+#ifdef CMSIS_FILTER_SCALAR
 #include "cmsis_scalar_filter.h"
 
-#ifdef __APPLE__
+#endif
+
+#ifdef VDSP_FILTER
 #include "vdsp_filter.h"
 #endif
 
@@ -17,53 +22,72 @@ using namespace ankerl;
 using namespace std::chrono_literals;
 
 template <typename T>
-void RunFilter(std::span<const float> input, std::span<float> output)
+void RunFilter(std::span<const float> input, std::span<float> output, size_t block_size)
 {
     T filter;
-    filter.process(input, output);
+
+    assert(input.size() % block_size == 0);
+
+    size_t block_count = input.size() / block_size;
+    for (size_t i = 0; i < block_count; ++i)
+    {
+        std::span<const float> input_block = input.subspan(i * block_size, block_size);
+        std::span<float> output_block = output.subspan(i * block_size, block_size);
+
+        filter.process(input_block, output_block);
+    }
+}
+
+template <typename T>
+void RunTest(const std::string& name)
+{
+    const std::vector<size_t> kBlockSize = {1, 4, 8, 16, 32, 64, 128, 256, 512, 1024};
+    constexpr size_t input_size = 4096;
+
+    nanobench::Bench bench;
+    bench.title(name);
+    bench.relative(true);
+    bench.warmup(100);
+    bench.batch(input_size);
+    bench.unit("samples");
+
+    std::vector<float> input(input_size, 0);
+
+    // Fill with white noise
+    std::default_random_engine generator;
+    std::normal_distribution<double> dist(0, 0.1);
+    for (size_t i = 0; i < input.size(); ++i)
+    {
+        input[i] = dist(generator);
+    }
+
+    std::vector<float> output(input_size, 0);
+
+    for (size_t i = 0; i < kBlockSize.size(); ++i)
+    {
+        std::string test_name = name + "_" + std::to_string(kBlockSize[i]);
+        bench.run(test_name, [&]() { RunFilter<T>(input, output, kBlockSize[i]); });
+    }
+
+    std::string filename = "perf_results_" + name + ".json";
+    std::ofstream render_out(filename);
+    bench.render(ankerl::nanobench::templates::json(), render_out);
 }
 
 int main()
 {
-    constexpr std::array kBlockSize = {1, 4, 8, 16, 32, 64, 128, 256, 512, 1024};
-    nanobench::Bench bench;
-    bench.title("Filter Benchmark");
-    bench.relative(true);
-    bench.warmup(100);
+    std::cout << "Running filter benchmarks..." << std::endl;
 
-    for (size_t i = 0; i < kBlockSize.size(); ++i)
-    {
-        size_t block_size = kBlockSize[i];
-        std::vector<float> input(block_size, 0);
-
-        bench.minEpochIterations((1024 / block_size) * 1000);
-        // Fill with white noise
-        std::default_random_engine generator;
-        std::normal_distribution<double> dist(0, 0.1);
-        for (size_t i = 0; i < input.size(); ++i)
-        {
-            input[i] = dist(generator);
-        }
-
-        std::vector<float> output(block_size, 0);
-
-        // bench.timeUnit(1us, "us");
-        bench.batch(block_size);
-        bench.unit("samples");
-
-        bench.run("CMSIS Scalar DF2T", [&]() { RunFilter<CMSISScalarFilterDF2T>(input, output); });
-        bench.run("CMSIS Scalar DF1", [&]() { RunFilter<CMSISScalarFilterDF1>(input, output); });
-        bench.run("Basic Filter", [&]() { RunFilter<BasicFilter>(input, output); });
-        bench.run("Cascaded IIR DF2T", [&]() { RunFilter<CascadedIIRDF2T>(input, output); });
-        bench.run("Cascaded IIR DF1", [&]() { RunFilter<CascadedIIRDF1>(input, output); });
-
-#ifdef __APPLE__
-        bench.run("vDSP Filter", [&]() { RunFilter<vDSPFilter>(input, output); });
+#ifdef BASIC_FILTER
+    RunTest<BasicFilter>("basic_filter");
 #endif
-    }
-    std::ofstream render_out("perf_results.json");
-    bench.render(ankerl::nanobench::templates::json(), render_out);
 
-    std::ofstream html_out("perf_results.html");
-    bench.render(ankerl::nanobench::templates::htmlBoxplot(), html_out);
+#ifdef CMSIS_FILTER_SCALAR
+    RunTest<CMSISScalarFilterDF2T>("CMSISScalarFilterDF2T");
+    RunTest<CMSISScalarFilterDF1>("CMSISScalarFilterDF1");
+#endif
+
+#ifdef VDSP_FILTER
+    RunTest<VDSPFilter>("vdsp_filter");
+#endif
 }
